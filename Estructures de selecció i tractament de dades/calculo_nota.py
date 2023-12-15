5e1eaad3b3924620842f4af55d1fa6c56962411c
laasso <nrubialesguerrero@gmail.com>
2023-12-15 09:58:57 +0100

    prueba_op3

--- Estructures de selecció i tractament de dades/calculo_nota.py
+++ Estructures de selecció i tractament de dades/calculo_nota.py
@@ -12,8 +12,9 @@ pornota3 = int(30)
 media_min_op2 = int(7)
 
 alumnos_op_2 = []
+alumnos_op_3 = []
 
-
+nota1 = 0
 #la lista alumnos_info es provisonal es para no tener que poner todo el rato los datos XD
 alumnos_info = [["nicolas","rubiales", 18, 10, 10, 10],["iker", "macias", 19, 10, 10, 10],["carlos", "bonilla", 18, 10, 10, 9],["izan", "lozano",18, 8, 7, 5]]
 '''
@@ -21,7 +22,7 @@ print(alumnos_info)
 opcion_usuario = int(input('Introduce que deseas hacer '))
 
 '''
-
+'''
 for alumno in range(0,len(alumnos_info)):
     for info in range(3,len(alumnos_info)):
         suma_media_op2.append(alumnos_info[alumno][info] * pornota1 / 100)
@@ -34,3 +35,14 @@ for alumno in range(0,len(alumnos_info)):
         suma_media_op2 = []
 
 print(alumnos_op_2,"hola")
+'''
+
+
+for alumno in range(0,len(alumnos_info)):
+    for nota in range(3,len(alumnos_info)):
+        print(alumnos_info[alumno][nota])
+        if alumnos_info[alumno[nota]] > 8:
+            print("f")
+            alumnos_op_3.append(alumnos_info[0])
+
+print(alumnos_op_3,"hola")
