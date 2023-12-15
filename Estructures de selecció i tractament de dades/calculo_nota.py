080d8b2ac2a40cd545b6b693a910eec430d0ca21
laasso <nrubialesguerrero@gmail.com>
2023-12-15 08:42:58 +0100

    var alumnos_info

---
+++ Estructures de selecció i tractament de dades/calculo_nota.py
@@ -0,0 +1,7 @@
+'''
+ (Nom, Cognom, Edat, Nota1, Nota2, Nota3).
+'''
+#//PRE Usuario introduce datos
+
+#la lista alumnos_info es provisonal es para no tener que poner todo el rato los datos XD
+alumnos_info = [["nicolas","rubiales", 18, 10, 10, 10],["iker", "macias", 19, 5, 4, 10],["carlos", "bonilla", 18, 0, 10, 9],["izan", "lozano",18, 8, 7, 5] ]
