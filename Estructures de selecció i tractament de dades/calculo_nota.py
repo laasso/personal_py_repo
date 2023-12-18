083b40336141050f780217f8900d02bd7621f017
laasso <nrubialesguerrero@gmail.com>
2023-12-18 13:22:44 +0100

    op2funciona

--- Estructures de selecció i tractament de dades/calculo_nota.py
+++ Estructures de selecció i tractament de dades/calculo_nota.py
@@ -5,6 +5,8 @@
 
 suma_media_op2 = []
 
+checker = False
+
 pornota1 = int(30)
 pornota2 = int(40)
 pornota3 = int(30)
@@ -16,31 +18,38 @@ alumnos_op_3 = []
 
 nota1 = 0
 #la lista alumnos_info es provisonal es para no tener que poner todo el rato los datos XD
-alumnos_info = [["nicolas","rubiales", 18, 1, 8, 7],["iker", "macias", 19, 10, 10, 10],["carlos", "bonilla", 18, 10, 10, 9],["izan", "lozano",18, 8, 8, 5]]
+alumnos_info = [["nicolas","rubiales", 18, 1, 9, 7],["iker", "macias", 19, 7, 10, 10],["carlos", "bonilla", 18, 10, 10, 9],["izan", "lozano",18, 8, 8, 5]]
 '''
 print(alumnos_info)
 opcion_usuario = int(input('Introduce que deseas hacer '))
 
 '''
-'''
+
 for alumno in range(0,len(alumnos_info)):
-    for info in range(3,len(alumnos_info)):
+    for info in range(3,len(alumnos_info[alumno])):
         suma_media_op2.append(alumnos_info[alumno][info] * pornota1 / 100)
         suma_media_op2.append(alumnos_info[alumno][info] * pornota2 / 100)
         suma_media_op2.append(alumnos_info[alumno][info] * pornota3 / 100)
-        suma_media_op2 = sum(suma_media_op2)
-        if suma_media_op2 > media_min_op2:
+        suma_total_media_op2 = sum(suma_media_op2)
+        if suma_total_media_op2 > media_min_op2 & checker == False:
             alumnos_op_2.append(alumnos_info[alumno][0])
-            suma_media_op2 = []
+            checker = True
         suma_media_op2 = []
-
+    checker = False
+    
+checker = False
+alumnos_op_2 = tuple(alumnos_op_2)
 print(alumnos_op_2,"hola")
-'''
+
+
+
 
 for alumno in range(0,len(alumnos_info)):
-    for nota in range(3,len(alumnos_info)):
-        if alumnos_info[alumno][nota] > 8:
+    for nota in range(3,len(alumnos_info[alumno])):
+        if alumnos_info[alumno][nota] > 8 & checker == False:
             alumnos_op_3.append(alumnos_info[alumno][0])
-
-print(alumnos_op_3,)
+            checker = True
+    checker = False
+alumnos_op_3 = tuple(alumnos_op_3)
+print(alumnos_op_3)
 
