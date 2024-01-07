a77429bd6fedfd984d6b330ba72d984e8607035a
laasso <nrubialesguerreo@gmail.com>
2024-01-07 14:53:38 +0100

    2(7/1/24)RandomNotWOrking

--- Q&A/q&q.py
+++ Q&A/q&q.py
@@ -9,11 +9,34 @@ llista_preguntes = [
 ]
 
 score = 0
+opcio = 1
+seed = 3
 
-
-seed = 2
 random = (seed * 503) % 1000 / 100
 numero_pregunta = int(random * (len(llista_preguntes)))
 
-resposta = str(input((llista_preguntes[numero_pregunta])))
-print(resposta)
+
+print("""
+Benvingut al joc de preguntes i respostes
+Cada resposta correcta et suma un punt
+      """)
+
+while opcio != 0:
+    opcio = int(input(print("""
+Selecciona una opcio:
+    0. Sortir
+    1. Respondre
+      """)))
+    if opcio == 1:
+        random = (seed * 503) % 1000 / 100
+        numero_pregunta = int(random * (len(llista_preguntes)))
+        resposta = str(input((llista_preguntes[numero_pregunta])))
+        if resposta == llista_preguntes[numero_pregunta + 1]:
+            print("Resposta correcta!")
+            score = score + 1
+            print("La teva puntuacio:",score)
+        else:
+            print("Resposta incorrecta")
+            print("La teva puntuacio:",score)
+print("La teva puntuacio final es:",score)
+
