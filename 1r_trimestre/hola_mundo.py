3b51c3286e119c1189f0fd394b0a1049ad35b1a9
lasso <123463530+laasso@users.noreply.github.com>
2024-02-01 12:22:23 +0100

    Delete 1r_trimestre directory

--- 1r_trimestre/hola_mundo.py
+++
@@ -1,9 +0,0 @@
-print("hola mundom, this is a new commit fgrom github")
-for i in range(0,5):
-    print('50 copy')
-    
-   
-print("alberrt corrige los examenes")
-for i in range(0, 50): 
-    print('50 copy')
-    
