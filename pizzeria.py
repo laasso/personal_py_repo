826fdc841f98594360fb90008d176c198c8c8cdf
laasso <nrubialesguerrero@gmail.com>
2024-02-01 10:45:18 +0100

    Missing, stock manage and ingredient substraxct.

--- pizzeria.py
+++ pizzeria.py
@@ -6,15 +6,21 @@ Recordeu fer servir disseny top down, i mantenir un baix acoblament i una alta c
 
 '''
 order = []
+
 pizza_prepare = []
+
 stock = [{"Name":"Tomato", "Quantity":2},{"Name":"Cheese", "Quantity":3},{"Name":"Pepperoni", "Quantity":5}]
+
 pizza_options = [{"Name":"Margarita", "Ingredients":["tomato", "cheese"],"Price":10},
                  {"Name":"Pepperoni", "Ingredients":["tomato", "cheese", "pepperoni"],"Price":12},]
 count_pizza = 0
 
+ticket = float(0)
+
+
 def order_request(order,pizza_options, count_pizza):
     aproved = False
-    request = input("Order")
+    request = input("Order: ")
     while aproved == False :
         if count_pizza < len(pizza_options):
             if request in pizza_options[count_pizza]["Name"]:
@@ -26,19 +32,25 @@ def order_request(order,pizza_options, count_pizza):
             aproved = True
 
 
-def pizza_preparation(order, pizza_prepare):
+def pizza_preparation(order, pizza_prepare,stock):
     if len(order) > 0 :
         for pizza in order:
             pizza_prepare.append(pizza)
+    for pizza in pizza_prepare:
+        for ingredients in pizza["Ingredients"]:
+            print("")
+            #here we substract the ingredient  used to make the pizza to the stock
 
 def stock_manage(stock):
     for ingredients in stock:
-        print(ingredients)
-
+        print("")
+            #here we're going to be able to add, consult or eliminate stock\
+        
 order_request(order,pizza_options,count_pizza)
 
-pizza_preparation(order,pizza_prepare)
+pizza_preparation(order,pizza_prepare,stock)
 
-print(order)
-print (pizza_prepare)
+for pizza in pizza_prepare:
+    ticket = pizza_prepare[0]["Price"]
 
+print(f"You requested the pizza {pizza_prepare[0]['Name']}, you have to pay {ticket}€")
