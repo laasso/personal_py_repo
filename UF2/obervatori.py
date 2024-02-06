7dde84136506d937e951716a388da1e348a2d031
laasso <nrubialesguerrero@gmail.com>
2024-02-06 10:59:45 +0100

    HAY QUE OCNVERITR STR A FLOAT

--- UF2/obervatori.py
+++ UF2/obervatori.py
@@ -1,13 +1,15 @@
 temp = [[2,[1,2,3,4,5,6,7]],[3,[7,6,5,4,3,2,1,]]]
 
-def FI(bool):
-    bool = False
-    return bool
 
-def RT(num):
-    
 exe_option = True
 exe_option_input = ''
+
+def RT(tlist):
+    temps = input()
+    aux_list = []
+    aux_list.append(temps)
+    print(aux_list)
+
 
 while exe_option == True:
     exe_option_input = (input('''Benvingut al registre de temperatures
@@ -18,6 +20,6 @@ while exe_option == True:
 [FI] Sortir.
 '''))
     if exe_option_input == 'FI':
-        FI(exe_option)
+        exe_option = False
     elif exe_option_input == 'RT':
         RT(temp)
