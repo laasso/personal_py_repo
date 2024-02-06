6a884d6d2ef5c4dac4d6e1d5eb25c8a31b520a74
laasso <nrubialesguerrero@gmail.com>
2024-02-06 10:48:02 +0100

    fi a func

--- UF2/obervatori.py
+++ UF2/obervatori.py
@@ -1,8 +1,14 @@
 temp = [[2,[1,2,3,4,5,6,7]],[3,[7,6,5,4,3,2,1,]]]
 
+def FI(bool):
+    bool = False
+    return bool
 
+def RT(num):
+    
 exe_option = True
 exe_option_input = ''
+
 while exe_option == True:
     exe_option_input = (input('''Benvingut al registre de temperatures
 -------------------------------------
@@ -12,5 +18,6 @@ while exe_option == True:
 [FI] Sortir.
 '''))
     if exe_option_input == 'FI':
-        exe_option = False
-        print('works')
+        FI(exe_option)
+    elif exe_option_input == 'RT':
+        RT(temp)
