8d6343f7fd457f2c8574b38ba39ec62dc9dd5e77
laasso <nrubialesguerrero@gmail.com>
2024-02-06 10:38:49 +0100

    Comprovem sortida

--- UF2/obervatori.py
+++ UF2/obervatori.py
@@ -4,10 +4,13 @@ temp = [[2,[1,2,3,4,5,6,7]],[3,[7,6,5,4,3,2,1,]]]
 exe_option = True
 exe_option_input = ''
 while exe_option == True:
-    exe_option_input(input('''
+    exe_option_input = (input('''Benvingut al registre de temperatures
+-------------------------------------
 [RT] Registrar temperatures setmanals. 
 [MJ] Consultar temperatura mitjana. 
 [DF] Consultar diferència màxima. 
 [FI] Sortir.
 '''))
-    
+    if exe_option_input == 'FI':
+        exe_option = False
+        print('works')
