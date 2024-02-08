ea4b655ed3fd12f53e1201b57efef1b71170d2ba
laasso <nrubialesguerrero@gmail.com>
2024-02-08 08:47:03 +0100

    ARRAYS OK

--- UF2/obervatori.py
+++ UF2/obervatori.py
@@ -1,23 +1,23 @@
 temp = []
 day_counter = 0
-
+week_counter = 0
 exe_option = True
 exe_option_input = ''
 
-def RT(tlist,counter):
+def RT(tlist,counter,wk_counter):
+    wk_counter = wk_counter + 1
     aux_list = []
     aux_list = input()
     counter = counter + 7
-    print(aux_list)
     aux_list = aux_list.split(' ')
     float_list = []
     for num in aux_list:
         float_list.append(float(num))
+    print(tlist)
+    tlist.append([wk_counter, float_list])  
+    print(wk_counter)
+    print(tlist)
 
-    print(float_list)
-
-
-#hacer split para separar en diferentes posicions los datos y usar un float() para convertirlos
     
 def dias(contador): 
     print(contador)
@@ -33,4 +33,5 @@ while exe_option == True:
     if exe_option_input == 'FI':
         exe_option = False
     elif exe_option_input == 'RT':
-        RT(temp,day_counter)
+        RT(temp,day_counter,week_counter)
+        week_counter = week_counter + 1
