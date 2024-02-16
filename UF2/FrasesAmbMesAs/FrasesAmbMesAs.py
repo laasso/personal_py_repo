06a95a047564c2be4d533bccbe5aef5b07ee6cfe
laasso <nrubialesguerrero@gmail.com>
2024-02-16 09:27:34 +0100

    mayusminus convertstrarray

--- UF2/FrasesAmbMesAs/FrasesAmbMesAs.py
+++ UF2/FrasesAmbMesAs/FrasesAmbMesAs.py
@@ -6,5 +6,13 @@ def printMostAsSentence():
     ...
 def mostAsSentence():
     ...
+def convertStrToArray(sentence):
+    sentence = ignoreMayusMinus(sentence)
+    sentence = list(sentence)
+def ignoreMayusMinus(sentence):
+    sentence = sentence.casefold()
+    return sentence
+
 def exitProgramm():
     ...
+
