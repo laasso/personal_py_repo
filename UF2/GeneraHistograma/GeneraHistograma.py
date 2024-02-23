054d4a27ff025e09a10492b959e8e2acd2e4746b
laasso <nrubialesguerrero@gmail.com>
2024-02-23 08:30:01 +0100

    carrega funcions

---
+++ UF2/GeneraHistograma/GeneraHistograma.py
@@ -0,0 +1,21 @@
+CARES_DAU:int = 6
+
+llista_tirades:list[int] = [0] * (CARES_DAU * 2 - 1)
+
+def generar_histograma() -> None:
+    generar_tirades()
+    mostrar_histograma()
+    mostrar_maximo()
+
+def generar_tirades():
+    ...
+
+def generar_tirades():
+    ...
+
+def mostrar_histograma():
+    ...
+def mosntrar_maximo():
+    ...
+
+generar_histograma()
