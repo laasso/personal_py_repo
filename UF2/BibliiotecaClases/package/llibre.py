7bf87c2653fffd2f1a767c62626a3d1fa7abae3d
laasso <nrubialesguerrero@gmail.com>
2024-03-19 10:38:54 +0100

    class libro

--- UF2/BibliiotecaClases/package/llibre.py
+++ UF2/BibliiotecaClases/package/llibre.py
@@ -1,4 +1,18 @@
 class Llibre():
-    def __init__(self) -> list:
-        llibres: list = ['El libro de la selva', 'Rubiales', 3]
-        
+    def __init__(self, titulo, autor:str, ejemplares:int):
+        self.titulo = titulo
+        self.autor  = autor
+        self.ejemplares = ejemplares
+
+def prestar(self):
+    if self.ejemplares > 0:
+        self.ejemplares -= 1
+        print(f'Llibre prestat: {self.titulo}, Autor: {self.autor}')
+    else:
+        print('No hay ejemplares')
+
+def devolver(self):
+    self.ejemplares += 1
+
+def informacion(self):
+    print(f'Llibre: {self.titulo}, Autor: {self.autor}, Ejemplares{self.ejemplares}')
