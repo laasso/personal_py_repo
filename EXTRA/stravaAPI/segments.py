d2822311670059834a0f7594ca7a50cd886749bd
laasso <nrubialesguerrero@gmail.com>
2024-05-07 12:56:23 +0200

    CHANGES

--- EXTRA/stravaAPI/segments.py
+++ EXTRA/stravaAPI/segments.py
@@ -3,10 +3,12 @@ import time
 import strava_api_v3
 from strava_api_v3.rest import ApiException
 from pprint import pprint
+import os
+from dotenv import load_dotenv
 
 # Configure OAuth2 access token for authorization: strava_oauth
 configuration = strava_api_v3.Configuration()
-configuration.access_token = 'cdb937b5c57b9bbcc828fa6714bd368304988946'
+configuration.access_token = os.getenv("ACCESS_TOKEN")
 
 # create an instance of the API class
 api_instance = strava_api_v3.SegmentsApi(strava_api_v3.ApiClient(configuration))
